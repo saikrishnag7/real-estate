6fd49be6723ba46aa09a503e723972db445324b9
Sai Krishna <saikrishna.gajjarapu@gmail.com>
2024-06-05 15:43:06 +0530

    created a error handler middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const signin = async(req,res) =>{
+export const signin = async(req,res,next) =>{
    const {username ,email ,password} = req.body;
    const hashPassword = bcrypt.hashSync(password,10);
    const newUser = new User({username , email ,hashPassword});
@@ -9,7 +9,7 @@ export const signin = async(req,res) =>{
     res.status(201).json("new user created"); 
    }
    catch(error){
-    res.status(500).json(error.message);
+      next(error);
    }
 
 
